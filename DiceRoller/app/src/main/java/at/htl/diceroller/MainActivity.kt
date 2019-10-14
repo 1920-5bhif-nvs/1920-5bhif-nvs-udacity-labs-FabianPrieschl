2107462e052d0957d2c1efdcae74b3cb61fa645a
FabianPrieschl <prieschlfabian@gmail.com>
2019-10-14 12:34:31 +0200

    Added random dice roll

--- DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/at/htl/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package at.htl.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
-            Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT)
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+
+        resultText.text = randomInt.toString()
     }
 }
