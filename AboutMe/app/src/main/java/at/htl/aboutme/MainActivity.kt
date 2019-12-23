db46cf78a399cf0ac5e7d6f3bd360a53ead5e50c
FabianPrieschl <prieschlfabian@gmail.com>
2019-12-23 12:28:27 +0100

    Added code for nickname edit - Fabian Prieschl

--- AboutMe/app/src/main/java/at/htl/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/at/htl/aboutme/MainActivity.kt
@@ -1,12 +1,35 @@
 package at.htl.aboutme
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.name_text)
+
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val inm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
