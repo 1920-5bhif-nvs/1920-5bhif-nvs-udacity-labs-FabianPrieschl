aec1c56ae7935840ffc96d1f9100b17827c9ed67
FabianPrieschl <prieschlfabian@gmail.com>
2020-02-17 15:54:51 +0100

    Added MarsWebService contact - Prieschl

--- Lesson8/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Lesson8/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,16 +20,18 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.android.marsrealestate.network.MarsApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
  */
 class OverviewViewModel : ViewModel() {
 
-    // The internal MutableLiveData String that stores the status of the most recent request
     private val _response = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status String
     val response: LiveData<String>
         get() = _response
 
@@ -44,6 +46,14 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        _response.value = "Set the Mars API Response here!"
+        MarsApi.retrofitService.getProperties().enqueue( object: Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                _response.value = "Failure: " + t.message
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                _response.value = response.body()
+            }
+        })
     }
 }
